6b928724181f31ae76565d54ffbbe5a41707c0c2
jackson <jackson.joseph@coherent.in>
2026-09-23 17:01:36 +0530

    Update MongoDB connection timeout

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb+srv://havocjack2942002_db_user:FwCaEfMwzG4OfroL@cluster0.grekmpf.mongodb.net/?appName=Cluster0");
+
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log("MongoDB Connection Failed:", error.message);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://havocjack2942002_db_user:FwCaEfMwzG4OfroL@cluster0.grekmpf.mongodb.net/?appName=Cluster0");
+    await mongoose.connect("mongodb+srv://havocjack2942002_db_user:FwCaEfMwzG4OfroL@cluster0.grekmpf.mongodb.net/?appName=Cluster0", {
+      serverSelectionTimeoutMS: 5000,
+    });
 
     console.log("MongoDB Connected");
   } catch (error) {
